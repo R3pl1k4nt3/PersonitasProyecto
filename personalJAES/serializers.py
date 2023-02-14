f395bdc85d10a81b60fba1d79cf684abb86af66e
Alex <bktato@gmail.com>
2023-02-14 08:53:37 +0100

    cambio el serializer

--- personalJAES/serializers.py
+++ personalJAES/serializers.py
@@ -1,22 +1,22 @@
 from rest_framework import serializers
 from .models import *
 
-class UnidadSerializer(serializers.ModelSerializer):
+class UnidadSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Unidad
         fields = ('unidad',)
 
-class DepartamentoSerializer(serializers.ModelSerializer):
+class DepartamentoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Departamento
         fields = ('departamento', 'unidad')
         
-class NegociadoSerializer(serializers.ModelSerializer):
+class NegociadoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Negociado
         fields = ('negociado', 'departamento')
 
-class PersonaSerializer(serializers.ModelSerializer):
+class PersonaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Persona
         fields = ('empleo','nombre','apellidos','telefono','fecha_incorporacion','fotografia','unidad','departamento')
